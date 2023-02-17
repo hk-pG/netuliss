f7244f8fdd1f90b199eb2ca6473c368d8adfde4b
hk-p114514 <hukada.gogotea@gmail.com>
2023-02-17 21:01:48 +0900

    build success

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Button } from '.';
+import React from "react";
+import { render } from "@testing-library/react";
+import { Button } from ".";
 
-
-test('Enjoy Testing!', () => {
+test("Enjoy Testing!", () => {
   render(<Button />);
-})
+});
